bf422b3ccbbf83f84783d58fe6b1fd469bcd9af6
Shripal Kothari <61024483+shripalkothari1102@users.noreply.github.com>
2022-03-14 22:46:27 +0530

    subcategory and admin aproval updates

--- vats/models.py
+++ vats/models.py
@@ -7,7 +7,7 @@ import os
 
 class Category(models.Model):
 
-    name = models.CharField(_("Category"), max_length=50)
+    name = models.CharField(_("Name"), max_length=50)
 
     class Meta:
         verbose_name = _("Category")
@@ -19,8 +19,8 @@ class Category(models.Model):
 
 class Subcategory(models.Model):
 
-    category = models.ForeignKey("vats.Category", on_delete=models.SET_NULL,blank=True,null=True)
-    name = models.CharField(_("Subcategory"), max_length=50)
+    category = models.ForeignKey("vats.Category", on_delete=models.CASCADE)
+    name = models.CharField(_("Name"), max_length=50)
 
     class Meta:
         verbose_name = _("Subcategory")
@@ -37,7 +37,7 @@ class Ticket(models.Model):
     # ticket.assigned_to.id == request.user.id
     # ticket.created_by.id == request.user.id
     status_choice = (
-        ("Approval","Approval"),                # Admin     => Approve, Reject
+        ("Pending","Pending"),                  # Admin     => Approve with assignment, Reject with comment
         ("Assigned","Assigned"),                # Assigned to Manager, Created by Viewer   => Cancel ticket
         ("Scoping","Scoping"),                  # Assigned to Manager, Created by Viewer   => Cancel ticket
         ("In Progress","In Progress"),          # Assigned to Manager, Created by Viewer   => Cancel ticket
@@ -68,7 +68,7 @@ class Ticket(models.Model):
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True,)
     assigned_to = models.ForeignKey("registration.User",related_name=_("Tasks"), on_delete=models.SET_NULL,null=True,blank=True)
-    status = models.CharField(_("Status"), max_length=50,choices=status_choice,null=True,blank=True)    
+    status = models.CharField(_("Status"), max_length=50,choices=status_choice,null=True,blank=True)
    
     class Meta:
         verbose_name = _("Ticket")
@@ -89,18 +89,18 @@ class Ticket(models.Model):
                 str_zeros += "0"
             self.number = "TKT" + str_zeros + str(number)
             
-        account_sid = 'ACcad8c9cd24dc3fa5b6c96ce3cb9cee56'
-        auth_token = 'e43dbe58d768be26b75f9321aae75420'
-        client = Client(account_sid, auth_token)
+        # account_sid = 'ACcad8c9cd24dc3fa5b6c96ce3cb9cee56'
+        # auth_token = 'e43dbe58d768be26b75f9321aae75420'
+        # client = Client(account_sid, auth_token)
 
-        message = client.messages.create(
-                                    body='Hello, Your Ticket has been generated kindly check it in your portal',
-                                    from_='+17752567143',
-                                    # to=str(self.created_by.phone_number)
-                                    to='+917048850488'
-                                )
+        # message = client.messages.create(
+        #                             body='Hello, Your Ticket has been generated kindly check it in your portal',
+        #                             from_='+17752567143',
+        #                             # to=str(self.created_by.phone_number)
+        #                             to='+917048850488'
+        #                         )
 
-        print(message.sid)
+        # print(message.sid)
         return super(Ticket, self).save(*args, **kwargs)
     
     def is_open(self):
@@ -116,19 +116,19 @@ class Ticket(models.Model):
         date = self.updated_at + timedelta(days=0, hours=5, minutes=30)
         return date
 
-class WorkNotes(models.Model):
+class Worknote(models.Model):
 
-    ticket = models.ForeignKey("vats.Ticket",related_name="Worknotes", on_delete=models.CASCADE)
+    ticket = models.ForeignKey("vats.Ticket", on_delete=models.CASCADE)
     comments = models.TextField(_("Comments"))
     commented_by = models.ForeignKey("registration.User", on_delete=models.CASCADE)
     created_at = models.DateTimeField(_("Created Date/Time"), auto_now_add=True)
     
     class Meta:
-        verbose_name = _("WorkNotes")
-        verbose_name_plural = _("WorkNotess")
+        verbose_name = _("WorkNote")
+        verbose_name_plural = _("WorkNotes")
 
     def __str__(self):
         return str(self.ticket.created_by) + " - " + self.comments
 
     def get_absolute_url(self):
-        return reverse("WorkNotes_detail", kwargs={"id": self.id})
+        return reverse("WorkNote_detail", kwargs={"id": self.id})
